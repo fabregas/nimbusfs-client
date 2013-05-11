6d095134589a83750245396b52d9c6cfc029dad7
fabregas <kksstt@gmail.com>
2013-05-11 18:08:28 +0300

    appended ks_domain_controller.py

---
+++ id_client/webdav/ks_domain_controller.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+"""
+Copyright (C) 2013 Konstantin Andrusenko
+    See the documentation for further information on copyrights,
+    or contact the author. All Rights Reserved.
+
+@package id_client.webdav.ks_domain_controller
+@author Konstantin Andrusenko
+@date May 12, 2013
+"""
+
+from wsgidav.http_authenticator import SimpleDomainController
+from nimbus_client.core.logger import logger
+
+class KSDomainController(SimpleDomainController):
+    def __init__(self, key_storage):
+        self.key_storage = key_storage
+
+    def getDomainRealm(self, inputURL, environ):
+        """Resolve a relative url to the  appropriate realm name."""
+        return ''
+
+    def requireAuthentication(self, realmname, environ):
+        """Return True if this realm requires authentication or False if it is 
+        available for general access."""
+        if environ.get('REMOTE_ADDR', '') in ('127.0.0.1', 'localhost'):
+            return False
+        return True 
+    
+    def isRealmUser(self, realmname, username, environ):
+        """Returns True if this username is valid for the realm, False otherwise."""
+        return True
+ 
+    def getRealmUserPassword(self, realmname, username, environ):
+        """Return the password for the given username for the realm. 
+        Used for digest authentication.
+        """
+        raise Exception('Unsupported digest AUTH')
+      
+    
+    def authDomainUser(self, realmname, username, password, environ):
+        """Returns True if this username/password pair is valid for the realm, 
+        False otherwise. Used for basic authentication."""
+        return self.key_storage.validate(password)
--- tests/ideposit_client_class_test.py
+++ tests/ideposit_client_class_test.py
@@ -49,7 +49,7 @@ class TestIdepositbox(unittest.TestCase):
 
         TestIdepositbox.CLIENT.start(PASSWD)
 
-        self.assertEqual(TestIdepositbox.CLIENT.status, CS_STARTED)
+        self.assertEqual(TestIdepositbox.CLIENT.get_status(), CS_STARTED)
 
     def test01_create_dir(self):
         client = WebDAVClient("127.0.0.1", 8080)
@@ -82,7 +82,7 @@ class TestIdepositbox(unittest.TestCase):
         response = client.propfind("/foo", depth=1)
         self.assertTrue('displayname>test.out<' in response.content)
 
-        wait_oper_status(TestIdepositbox.CLIENT.nibbler.inprocess_operations, '/foo/test.out', Transaction.TS_FINISHED)
+        wait_oper_status(TestIdepositbox.CLIENT.get_nibbler().inprocess_operations, '/foo/test.out', Transaction.TS_FINISHED)
 
 
     def test04_get(self):
@@ -114,7 +114,7 @@ class TestIdepositbox(unittest.TestCase):
     def test99_stop(self):
         time.sleep(1.0)
         TestIdepositbox.CLIENT.stop()
-        self.assertEqual(TestIdepositbox.CLIENT.status, CS_STOPPED)
+        self.assertEqual(TestIdepositbox.CLIENT.get_status(), CS_STOPPED)
 
 
 
