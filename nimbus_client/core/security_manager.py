47dce517002729bbc04f7f57625a17a777bbf194
fabregas <kksstt@gmail.com>
2013-05-12 07:10:43 +0300

    os.system -> Popen in security_manager.py

--- nimbus_client/core/security_manager.py
+++ nimbus_client/core/security_manager.py
@@ -18,11 +18,13 @@ import struct
 import shutil
 from datetime import datetime
 from base64 import b64encode, b64decode
+from subprocess import Popen, PIPE, STDOUT
 
 from nimbus_client.core.constants import SPT_FILE_BASED, SPT_TOKEN_BASED
 from nimbus_client.core.encdec_provider import EncDecProvider
 from nimbus_client.core import pycrypto_enc_engine
 from nimbus_client.core.exceptions import InvalidPasswordException
+from nimbus_client.core.logger import logger
 
 CLIENT_CERT_FILENAME = 'client_certificate.pem'
 CLIENT_PRIKEY_FILENAME = 'client_prikey'
@@ -84,6 +86,21 @@ class FileBasedSecurityManager(AbstractSecurityManager):
         ##    return cls.KSS_INVALID
         return cls.KSS_EXISTS
 
+    def exec_openssl(self, command, stdin=None, cwd=None):
+        '''Run openssl command. PKI_OPENSSL_BIN doesn't need to be specified'''
+        c = ['openssl']
+        c.extend(command)
+
+        proc = Popen(c, shell=False, stdin=PIPE, stdout=PIPE, stderr=STDOUT, cwd=cwd)
+        stdout_value, stderr_value = proc.communicate(stdin)
+
+        out = stdout_value
+        if stderr_value:
+            out += '\n%s'%stderr_value
+        if proc.returncode != 0:
+            logger.debug('OpenSSL error: %s'%out)
+        return proc.returncode, out
+
     @classmethod
     def initiate_key_storage(cls, ks_path, ks_pwd):
         ks_path = os.path.abspath(ks_path)
@@ -103,28 +120,31 @@ class FileBasedSecurityManager(AbstractSecurityManager):
             raise Exception('Can not write to "%s"'%ks_path)
 
         pkey_file = tempfile.NamedTemporaryFile()
-        ret = os.system('openssl genrsa -out %s 1024'%pkey_file.name)
-        if ret:
+        retcode, out = self.exec_openssl(['genrsa', '-out', pkey_file.name, '1024'])
+        if retcode:
             raise Exception('Can not generate private key using openssl command')
 
-        ret = os.system('openssl pkcs12 -export -inkey %s -nocerts -out %s -password pass:%s'%\
-                (pkey_file.name, ks_path, ks_pwd))
-        pkey_file.close()
-        if ret:
-            raise Exception('Can not create key chain at %s'%ks_path)
+        try:
+            retcode, out = self.exec_openssl(['pkcs12', '-export', '-inkey', pkey_file.name, \
+                '-nocerts', '-out', ks_path, '-password', 'stdin'], ks_pwd)
+            if retcode:
+                raise Exception('Can not create key chain at %s'%ks_path)
+        finally:
+            pkey_file.close()
 
     def _load_key_storage(self, ks_path, passwd):
         if not os.path.exists(ks_path):
             raise Exception('Key chain file %s does not found!'%ks_path)
 
         tmp_file = tempfile.NamedTemporaryFile()
-        ret = os.system('openssl pkcs12 -in %s -out %s -password pass:%s -nodes'%\
-                (ks_path, tmp_file.name, passwd))
-        if ret:
-            raise InvalidPasswordException('Can not open key chain! Maybe password is invalid!')
+        try:
+            retcode, out = self .exec_openssl(['pkcs12', '-in', ks_path, '-out', tmp_file.name, '-password', 'stdin', '-nodes'], passwd)
+            if retcode:
+                raise InvalidPasswordException('Can not open key chain! Maybe pin-code is invalid!')
+            data = open(tmp_file.name).read()
+        finally:
+            tmp_file.close()
 
-        data = open(tmp_file.name).read()
-        tmp_file.close()
         pkey_s = re.search('(-----BEGIN PRIVATE KEY-----(\w|\W)+-----END PRIVATE KEY-----)', data)
         if not pkey_s:
             raise Exception('Private key does not found in key chain!')
@@ -139,16 +159,16 @@ class FileBasedSecurityManager(AbstractSecurityManager):
         cert_req_file = tempfile.NamedTemporaryFile()
         pkey_file.write(self._client_prikey)
         pkey_file.flush()
-        ret = os.system('openssl req -key %s -out %s -new -subj /CN=%s/O=iDepositBox\ software/OU=clients.idepositbox.com'%\
-                (pkey_file.name, cert_req_file.name, cert_cn))
-
-        if ret:
-            raise Exception('No certificate request generated!')
-        cert_req = open(cert_req_file.name).read()
-        pkey_file.close()
-        cert_req_file.close()
+        try:
+            retcode, out = self.exec_openssl(['req', '-key', pkey_file.name, '-out', cert_req_file.name, \
+                '-new', '-subj', '/CN=%s/O=iDepositBox\ software/OU=clients.idepositbox.com'%cert_cn])
+            if retcode:
+                raise Exception('No certificate request generated!\n%s'%out)
+            cert_req = open(cert_req_file.name).read()
+        finally:
+            pkey_file.close()
+            cert_req_file.close()
         return cert_req
-
 
     def append_certificate(self, ks_path, ks_pwd, cert):
         pkey_file = tempfile.NamedTemporaryFile()
@@ -158,16 +178,20 @@ class FileBasedSecurityManager(AbstractSecurityManager):
         cert_file.write(cert)
         cert_file.flush()
 
-        ret = os.system('openssl pkcs12 -export -inkey %s -in %s -out %s -password pass:%s'%\
-                (pkey_file.name, cert_file.name, ks_path, ks_pwd))
-
-        pkey_file.close()
-        cert_file.close()
-        if ret:
-            raise Exception('Can not update key chain at %s'%ks_path)
+        try:
+            retcode, out =  self.exec_openssl(['pkcs12', '-export', \
+                    '-inkey', pkey_file.name, '-in', cert_file.name, '-out', ks_path, \
+                    '-password', 'stdin'], ks_pwd)
+            if retcode:
+                raise Exception('Can not update key chain at %s'%ks_path)
+        finally:
+            pkey_file.close()
+            cert_file.close()
 
     def validate(self, password):
-        ret = os.system('openssl pkcs12 -in %s -password pass:%s -info -noout -nodes'%(self._ks_path, password))
-        if ret:
+        retcode, out = self.exec_openssl(['pkcs12', '-in', self._ks_path, \
+                '-password', 'stdin', '-info', '-noout', '-nodes'], password)
+        if retcode:
             return False
         return True
+
