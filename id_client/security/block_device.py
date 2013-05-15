5b22c1e4994b521f40e2371a23f648cc699b25eb
fabregas <kksstt@gmail.com>
2013-05-15 13:06:55 +0300

    appended base routines for block device managing

---
+++ id_client/security/block_device.py
@@ -0,0 +1,201 @@
+#!/usr/bin/python
+"""
+Copyright (C) 2013 Konstantin Andrusenko
+    See the documentation for further information on copyrights,
+    or contact the author. All Rights Reserved.
+
+@package id_client.security.block_device
+@author Konstantin Andrusenko
+@date May 15, 2013
+"""
+import struct
+
+DATA_START_SEEK = 2050 * 512 #start at 2050 sector 
+
+def read_ub(data):
+    # Read an unsigned byte from data block
+    return struct.unpack('B', data[0])[0]
+  
+def read_us(data):
+    # Read an unsigned short int (2 bytes) from data block    
+    return struct.unpack('<H', data[0:2])[0]
+
+def read_ui(data):
+    # Read an unsigned int (4 bytes) from data block    
+    return struct.unpack('<I', data[0:4])[0]
+
+class PartitionEntry:
+    def __init__(self):
+        self.status = 0x00
+        self.start_head = 0x00
+        self.start_sector = 0x00
+        self.start_cylinder = 0x00
+        self.part_type = 0x00
+        self.end_head = 0x00
+        self.end_sector = 0x00
+        self.end_cylinder = 0x00
+        self.LBA = 0x00
+        self.num_sectors = 0x00
+
+    def generate(self):
+        start_sc_spl = ((self.start_sector & 0x3F) | ((self.start_cylinder >> 2) & 0xC0)) | ((self.start_cylinder & 0xFF) << 8)
+        ebs_sc_spl = ((self.end_sector & 0x3F) | ((self.end_cylinder >> 2) & 0xC0)) | ((self.end_cylinder & 0xFF) << 8)
+
+        return struct.pack('<BBHBBHII', self.status, self.start_head, start_sc_spl, self.part_type,
+                self.end_head, ebs_sc_spl, self.LBA, self.num_sectors)
+
+    def parse(self, data):
+        self.status = read_ub(data)
+        self.start_head = read_ub(data[1])
+        tmp = read_ub(data[2])
+        self.start_sector = tmp & 0x3F
+        self.start_cylinder = (((tmp & 0xC0)>>6)<<8) + read_ub(data[3])
+        self.part_type = read_ub(data[4])
+        self.end_head = read_ub(data[5])
+        tmp = read_ub(data[6])
+        self.end_sector = tmp & 0x3F
+        self.end_cylinder = (((tmp & 0xC0)>>6)<<8) + read_ub(data[7])
+        self.LBA = read_ui(data[8:12])
+        self.num_sectors = read_ui(data[12:16])    
+    
+    def print_partition(self):
+        self.check_status()
+        print "CHS of first sector: %d %d %d" % \
+            (self.start_cylinder, self.start_head, self.start_sector)
+        print "Part type: 0x%02X" % self.part_type
+        print "CHS of last sector: %d %d %d" % \
+            (self.end_cylinder, self.end_head, self.end_sector)
+        print "LBA of first absolute sector: %d" % (self.LBA)
+        print "Number of sectors in partition: %d" % (self.num_sectors)
+                
+    def check_status(self):
+        if (self.status == 0x00):
+            print 'Non bootable'
+        else:
+            if (self.status == 0x80):
+                print 'Bootable'
+            else: 
+                print 'Invalid bootable byte'
+        
+# Table of four primary partitions        
+class PartitionTable:
+    def __init__(self):
+        self.partitions = [PartitionEntry() for i in range (0, 4)]
+
+    def parse(self, data):
+        for i in range(0, 4):
+            self.partitions[i].parse(data[16*i:16*(i+1)])
+
+    def generate(self):
+        return struct.pack('<16s16s16s16s', *[p.generate() for p in self.partitions])
+
+
+# Master Boot Record        
+class MBR:
+    def __init__(self):
+        self.boot_code = '\x00'*440
+        self.disk_sig = 0x0A0CFF09
+        self.partition_table = PartitionTable()
+        self.MBR_sig = 0xAA55
+
+    def generate(self):
+        return struct.pack('<440sIH64sH', self.boot_code, self.disk_sig, 0, self.partition_table.generate(), self.MBR_sig)
+
+    def parse(self, data):
+        self.boot_code = data[:440]        
+        self.disk_sig = read_ui(data[440:444])
+        unused = data[444:446]        
+        self.partition_table.parse(data[446:510])        
+        self.MBR_sig = read_us(data[510:512])
+        
+    def check_mbr_sig(self):
+        mbr_sig = self.MBR_sig
+        if (mbr_sig == 0xAA55):
+            return True
+        else:
+            return False
+            
+    def get_disk_sig(self):        
+        return self.disk_sig      
+                      
+
+
+class BlockDevice:
+    def __init__(self, dev_path):
+        self.__dev_path = dev_path
+
+    def format(self):
+        self.check_removable()
+        self.unmount_partitions()
+        self.change_mbr()
+        self.restore_fat_partition()
+
+
+    def check_removable(self):
+        pass
+
+    def unmount_partitions(self):
+        pass
+
+    def change_mbr(self):
+        try:
+            data = open(self.__dev_path, 'rb').read(512)
+        except IOError, err:
+            raise Exception('Can not read MBR from block device: %s'(self.__dev_path, err))
+
+        master_br = MBR()
+        master_br.parse(data)
+        if not master_br.check_mbr_sig():
+            logger.info('No valid MBR found at device %s. Recreating it..'%self.__dev_path)
+            master_br = MBR()
+        part = master_br.partition_table.partitions[0]
+
+        part.start_head = 0
+        part.start_sector = 2
+        part.start_cylinder = 0
+        part.part_type = 0xDB
+        part.end_head = 34
+        part.end_sector = 32
+        part.end_cylinder = 0
+        part.LBA = 0x01
+        part.num_sectors = 2049
+
+        part = master_br.partition_table.partitions[1]
+        part.start_head = 35
+        part.start_sector = 63
+        part.start_cylinder = 0
+        part.part_type = 0xAB
+        part.end_head = 97
+        part.end_sector = 63
+        part.end_cylinder = 0
+        part.LBA = 2050
+        part.num_sectors = 2049
+
+        '''
+        Disk: /dev/disk2geometry: 981/128/63 [7913472 sectors]
+        Signature: 0xABA55
+         Starting       Ending
+          #: id  cyl  hd sec -  cyl  hd sec [     start -       size]
+          ------------------------------------------------------------------------
+           1: DB    0   0   2 -    0  34  32 [         1 -       2049] CPM/C.DOS/C*
+           2: AB    0  35  63 -    0  97  63 [      2050 -       2049] Darwin Boot 
+           3: 00    0   0   0 -    0   0   0 [         0 -          0] unused      
+           4: 00    0   0   0 -    0   0   0 [         0 -          0] unused 
+        '''
+
+        logger.debug("Disk signature: 0x%08X" % (master_br.get_disk_sig()))
+        #for partition in master_br.partition_table.partitions:
+        #    print ""
+        #    partition.print_partition() 
+
+        new_mbr = master_br.generate()
+        logger.info('MBR is updated at device %s'%self.__dev_path)
+        try:
+            open(self.__dev_path,'r+b').write(new_mbr)
+        except IOError, err:
+            raise Exception('Can not update MBR at block device: %s'%err)
+
+
+    def restore_fat_partition(self):
+        pass
+
---
+++ id_client/security/block_device_based_ks.py
@@ -0,0 +1,123 @@
+#!/usr/bin/python
+"""
+Copyright (C) 2013 Konstantin Andrusenko
+    See the documentation for further information on copyrights,
+    or contact the author. All Rights Reserved.
+
+@package id_client.security.block_device_based_ks
+@author Konstantin Andrusenko
+@date May 15, 2013
+"""
+import os
+import struct
+
+from nimbus_client.core.security_manager import FileBasedSecurityManager
+from id_client.security.block_device import BlockDevice, DATA_START_SEEK
+
+class KSSignature:
+    SIGN_STRUCT = '<6sH'
+    SIGN_LEN = struct.calcsize(SIGN_STRUCT)
+    SING_ID = 'OFFS01'
+
+    @classmethod
+    def dump(cls, ks_len):
+        return struct.pack(self.SIGN_STRUCT, self.SING_ID, ks_len)
+
+    def __init__(self, dumped=None):
+        self.sign_id = None
+        self.ks_len = None
+        if dumped:
+            sef.load(dumped)
+
+    def load(self, dumped):
+        self.sign_id, self.ks_len = struct.unpack(self.SIGN_STRUCT, dumped)
+
+    def is_valid(self):
+        if self.sign_id == self.SING_ID and self.ks_len > 0:
+            return True
+        return False
+
+
+
+class FileOnBlockDevice:
+    def __init__(self, path):
+        self.__path = path
+
+    def __open_dev(self, read_only=False):
+        flags = 'rb'
+        if not read_only:
+            flags += '+'
+        try:
+            return open(self.__path, flags)
+        except IOError:
+            raise IOError('Device %s does not opened for %s!'%(self.__path,\
+                    'read' if read_only else 'write'))
+
+    def __read_signature(self, fd):
+        try:
+            fd.seek(DATA_START_SEEK)
+            data = fd.read(KSSignature.SIGN_LEN)
+            return KSSignature(data)
+        except IOError:
+            raise IOError('Device %s can not be read!'%self.__path)
+
+    def __write(self, data):
+        fd = self.__open_dev()
+        try:
+            fd.seek(DATA_START_SEEK)
+            fd.write(KSSignature.dump(len(data)))
+            fd.write(data)
+        except IOError:
+            raise IOError('Key chain does not write to device %s!'%self.__path)
+        finally:
+            fd.close()
+
+    def exists(self):
+        if not os.path.exists(self.__path):
+            return False
+
+        try:
+            fd = self.__open_dev(read_only=True)
+        except IOError:
+            return False
+
+        try:
+            ks_sign = self.__read_signature(fd)
+            if ks_sign.is_valid():
+                return True
+            return False
+        finally:
+            fd.close()
+        
+    def create_empty(self):
+        self.__write('')
+
+    def copy_from(self, dest_file):
+        try:
+            data = open(path, 'rb').read()
+        except IOError:
+            raise IOError('Can not read from "%s"'%path)
+        self.__write(data)
+
+    def read(self):
+        fd = self.__open_dev(read_only=True)
+        sign = self.__read_signature(fd)
+        if not sign.is_valid():
+            raise Exception('Key chain does not found at %s'%self.__path)
+        try:
+            return fd.read(sign.ks_len)
+        except IOError:
+            raise IOError('Key chain can not be read from %s!'%self.__path)
+
+        
+
+class BlockDeviceBasedSecurityManager(FileBasedSecurityManager):
+    ks_file_class = FileOnBlockDevice
+
+    @classmethod
+    def initiate_key_storage(cls, ks_path, ks_pwd):
+        block_dev = BlockDevice(ks_path)
+        block_dev.format()
+
+        FileBasedSecurityManager.initiate_key_storage(ks_path, ks_pwd)
+
